13055ed87124eb7aff95036cdbc85f3e913f168f
Divyansh <divyanshupadhyay23aiml@rnsit.ac.in>
2025-11-17 03:06:10 +0530

    max occurance

---
+++ Practice_DSA/Max_occurance.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+char getMaxOccuringCharacter(string s){
+    int arr[26] = {0};
+
+    //create an array of count of characters
+    for(int i = 0; i<s.length(); i++){
+        char ch = s[i];
+        //lowercase
+        int number = 0;
+        if(ch>='a' && ch<='z'){
+            number = ch - 'a';
+        }
+        //uppercase
+        else{
+            number = ch - 'A';
+
+        }
+        arr[number]++;
+    }
+
+    //find maximum occuring character
+    int maxi = -1,ans = 0;
+    for(int i = 0; i<26; i++){
+        if (maxi < arr[i]){
+            ans = i;
+            maxi = arr[i];
+        }
+
+    }
+
+    char finalAns = 'a'+ans;
+    return finalAns;
+}
+
+int main(){
+
+    string s;
+    cin>> s;
+    cout<< getMaxOccuringCharacter(s)<<endl;
+    return 0;
+}

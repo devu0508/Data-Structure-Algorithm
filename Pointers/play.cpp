929574d5f5a9594ead73e3d08b2a1c0b5a17c9b9
Divyansh <divyanshupadhyay23aiml@rnsit.ac.in>
2025-11-21 01:36:05 +0530

    Pointers

---
+++ Pointers/play.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num = 5;
+    int a = num;
+    cout<< "a before: " << num <<endl;
+    a++;
+    cout<< "a after: " << num <<endl;
+
+
+    int *p = &num;
+    cout<< "before: " << num <<endl;
+    (*p)++;
+    cout<< "after: " << num << endl;
+
+    //copying a pointer
+    int *q = p;
+    cout<< p <<"_"<< q << endl;
+    cout<< *p <<"_"<< *q << endl;
+
+    //important concept
+    int i = 3;
+    int *t = &i;
+    // cout << (*t)++ << endl;
+    *t = *t+1;
+    cout << *t << endl;
+    cout <<"before t" << t << endl;
+    t = t+1;
+    cout <<"after t" << t << endl;
+
+
+
+
+    return 0;
+}

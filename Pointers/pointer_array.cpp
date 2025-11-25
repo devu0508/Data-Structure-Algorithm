99f719ae35a7f03832a6df8dd1f1d19427b25968
Divyansh <divyanshupadhyay23aiml@rnsit.ac.in>
2025-11-26 01:04:10 +0530

    that's all

---
+++ Pointers/pointer_array.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int arr[10] = {2,5,7};
+    // cout <<" address of first memory block is " << arr << endl;
+    // cout <<" address of first memory block is " << &arr[0] << endl;
+    // cout <<" value at first memory block is " << *arr << endl;
+    // cout <<" Add 1 to value at first memory block is " << *arr + 1 << endl;
+    // cout <<" value at second memory block is " << *(arr + 1) << endl;
+    // cout <<" Add 1 to value at first memory block is " << (*arr) + 1 << endl;
+
+    // int temp[10];
+    // cout << sizeof(temp) << endl;
+    // cout << sizeof(*temp) << endl;
+    // cout << sizeof(&temp) << endl;
+
+
+
+    // int *ptr = &temp[0];
+    // cout << sizeof(ptr) << endl;
+    // cout << sizeof(*ptr) << endl;
+    // cout << sizeof(&ptr) << endl;
+
+    // int a[20] = {1,2,3,5};
+    // cout << &a[0] << endl; //& opperator on array and pointer give diffrent address
+    // cout << &a << endl;
+    // cout << a << endl;
+
+    // int *p = &a[0];
+    // cout << p << endl;
+    // cout << *p << endl;
+    // cout << &p << endl;
+
+    int arr[10];
+
+    //ERROR
+    // arr = arr+1;
+
+    int *ptr = &arr[0];
+    cout << ptr << endl;
+    ptr = ptr + 1;
+    cout << ptr << endl;
+
+
+
+
+
+    return 0;
+}
